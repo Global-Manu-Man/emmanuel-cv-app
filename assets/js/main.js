f69d01337d8ecc1d6146dff7c06f2fa9fbcb51f4
emmanuel <sandoval.morales.emmanuel@gmail.com>
2025-10-18 00:41:03 -0600

    Refactor form submission handling and email validation; remove redundant HTML doctype declaration

--- assets/js/main.js
+++ assets/js/main.js
@@ -222,41 +222,41 @@ themeButton.addEventListener('click', () => {
 
 // ==================== CONTACT ====================
 // Validate email and message ok  
-
-            const $form = document.querySelector('form');
-                if($form){
-                    $form.addEventListener('submit', handleSubmit);
-                    console.log('mesagge' + $form);
-                    async function handleSubmit(event) {
-                    event.preventDefault();
-                    const form = new FormData(this);
-                    const response = await fetch(this.action, {
-                        method: this.method,
-                        body: form,
-                        headers: {
-                            'Accept': 'Application/json'
-                        }
-        
-                    })
-                    if (response.ok) {
-                        this.reset();
-                        alert('Gracias por contactarme');
-                    }
-        
+    const $form = document.querySelector('form');
+    if ($form) {
+        $form.addEventListener('submit', handleSubmit);
+        console.log('mesagge' + $form);
+        async function handleSubmit(event) {
+            event.preventDefault();
+            const form = new FormData(this);
+            const response = await fetch(this.action, {
+                method: this.method,
+                body: form,
+                headers: {
+                    'Accept': 'Application/json'
                 }
-                }
-
-
-                // Validate email de web site
-                    function validate(email) {
-                        var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-                        var okvalide = expReg.test(email);
-                        if (okvalide == true) {
-                           
-                        } else {
-                        }
-
-                    }
+    
+            })
+            if (response.ok) {
+                this.reset();
+    
+            }
+    
+        }
+    }
+    
+    
+    // Validate email de web site
+    function validate(email) {
+        var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        var okvalide = expReg.test(email);
+        if (okvalide == true) {
+    
+        } else {
+    
+        }
+    
+    }
 
 
 function googleTranslateElementInit() {
